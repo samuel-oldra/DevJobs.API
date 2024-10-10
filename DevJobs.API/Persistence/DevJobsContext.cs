008a069c6994adbf4ed2c9e7f00d6b16039764e2
Samuel B. Oldra <samuel.oldra@gmail.com>
2024-10-10 00:00:00 -0300

    Atualização de pacotes

--- DevJobs.API/Persistence/DevJobsContext.cs
+++ DevJobs.API/Persistence/DevJobsContext.cs
@@ -5,14 +5,19 @@ namespace DevJobs.API.Persistence
 {
     public class DevJobsContext : DbContext
     {
-        public DbSet<JobVacancy> JobVacancies { get; set; }
-
         public DbSet<JobApplication> JobApplications { get; set; }
+
+        public DbSet<JobVacancy> JobVacancies { get; set; }
 
         public DevJobsContext(DbContextOptions<DevJobsContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<JobApplication>(ja =>
+            {
+                ja.HasKey(ja => ja.Id);
+            });
+
             modelBuilder.Entity<JobVacancy>(jv =>
             {
                 jv.HasKey(jv => jv.Id);
@@ -21,11 +26,6 @@ namespace DevJobs.API.Persistence
                     .WithOne()
                     .HasForeignKey(ja => ja.IdJobVacancy)
                     .OnDelete(DeleteBehavior.Restrict);
-            });
-
-            modelBuilder.Entity<JobApplication>(ja =>
-            {
-                ja.HasKey(ja => ja.Id);
             });
         }
     }
